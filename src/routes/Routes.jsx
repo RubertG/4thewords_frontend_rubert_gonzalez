469a8dd42975c003ebc98d52ad0cf37397c033c7
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-03-03 18:45:37 -0500

    Agregar ruta para crear leyenda en el componente de rutas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router'
 import { Home } from '../features/legends/pages/Home'
+import { CreateLegend } from '../features/legends/pages/CreateLegend'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/crear-leyenda' element={<CreateLegend />} />
     </Routes>
   )
 }
