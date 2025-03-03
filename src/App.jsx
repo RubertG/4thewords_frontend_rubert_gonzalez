231c0276fa4e7f4c02bc31dc292527759cee87ab
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-03-03 11:41:05 -0500

    Agregar componente Toaster para mostrar notificaciones en la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Toaster } from "sonner";
 import { AppRoutes } from "./routes/routes";
 
 export default function App() {
   return (
     <main className="px-4 my-6 max-w-6xl mx-auto lg:px-10 lg:my-10">
       <AppRoutes />
+      <Toaster />
     </main>
   )
 }
