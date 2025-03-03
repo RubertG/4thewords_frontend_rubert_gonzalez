cc3e0f05ce62710669128ed07d21afd72926e3fd
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-03-03 10:42:42 -0500

    Actualizar el color de fondo del componente Badge a verde

--- src/components/ui/Badge.jsx
+++ src/components/ui/Badge.jsx
@@ -6,7 +6,7 @@ export const Badge = ({
   return (
     <span
       className={clsx(
-        "inline-block py-1 px-3 rounded-full text-white bg-blue-900 text-xs",
+        "inline-block py-1 px-3 rounded-full text-white bg-green-500 text-xs",
         className
       )}
     >
