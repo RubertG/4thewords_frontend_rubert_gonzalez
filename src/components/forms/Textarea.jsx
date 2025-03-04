275d7a965bc018624734849bea7a5d1c574dac31
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2025-03-03 19:36:51 -0500

    Agregar componente Textarea para mejorar la entrada de texto en formularios

---
+++ src/components/forms/Textarea.jsx
@@ -0,0 +1,8 @@
+export const Textarea = ({ className,...props }) => {
+  return (
+    <textarea
+      className={`px-3 py-1.5 border rounded-xl text-sm w-full border-gray-200 placeholder:text-gray-400 text-gray-900 outline-0 focus:border-blue-700 focus:ring-1 focus:ring-blue-700 transition-colors ${className}`}
+      {...props}
+    />
+  );
+}
